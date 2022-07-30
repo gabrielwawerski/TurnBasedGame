e7cd9d64f6cf2b38d1b70f470b023af112ec9e8e
4masm <gabriel.wawerski@gmail.com>
2022-07-30 19:18:41 +0200

    commit

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from enum import Enum
 
 
-class MenuEnum(Enum):
+class EMenu(Enum):
+    def __str__(self):
+        return str.title(str(self.name)).replace("_", " ")
+
+
+class MainMenu(EMenu):
     START = 1
     EXIT = 2
 
-    def __str__(self):
-        return str.title(str(self.name)).replace("_", " ")
+
+class Menu(EMenu):
+    pass
 
 
 class Character:
@@ -18,24 +24,38 @@ class Character:
 class Battle:
     def __init__(self, opponent1, opponent2):
         self.opponent1 = opponent1
-        self.opponnet2 = opponent2
+        self.opponent2 = opponent2
 
 
 class App:
     def __init__(self):
         self._run = True
+        self.version = "0.1"
 
     def run(self):
         while self._run:
-            pass
+            print(f'DragonFight v{self.version}')
+
+            if self.main_menu():
+                self.start()
+            else:
+                exit()
+
+    def main_menu(self):
+        selection = int(input("> "))
+
+        if selection is MainMenu.START.value:
+            return True
+        elif selection is MainMenu.EXIT.value:
+            print("exiting")
+            return False
+        else:
+            print("Not a valid selection.")
+            self.main_menu()
+
+    def start(self):
+        print("here")
 
 
 if __name__ == '__main__':
     App().run()
-
-
-
-
-
-
-
